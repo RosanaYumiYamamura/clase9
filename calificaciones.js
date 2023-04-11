caa5fe02324f2894e7d6e519c4e5aaeec539903f
Rosana Yumi <rosanayumiyamamura1@gmail.com>
2023-04-10 21:34:07 -0300

    first commit

---
+++ calificaciones.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var rls = require("readline-sync");
+var nota1 = rls.questionInt("ingrese nota 1: ");
+var nota2 = rls.questionInt("ingrese nota 2: ");
+var nota3 = rls.questionInt("ingrese nota 3: ");
+var nombreAlumno = rls.question("ingrese nombre alumno: ");
+var result = 0;
+while (nombreAlumno != "") {
+    result = nota1 * 0.1 + nota2 * 0.5 + nota3 * 0.40;
+    console.log(result);
+    nombreAlumno = rls.question("ingrese nombre alumno: ");
+    if (nombreAlumno != "") {
+        nota1 = rls.questionInt("ingrese nota 1: ");
+        nota2 = rls.questionInt("ingrese nota 2: ");
+        nota3 = rls.questionInt("ingrese nota 3: ");
+    }
+}
